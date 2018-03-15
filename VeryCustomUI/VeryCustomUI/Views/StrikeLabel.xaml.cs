c48dd0619944f859b06d465621774b1807276408
Francesco <francesco.colombo@admaiorastudio.com>
2018-03-16 00:08:10 +0100

    Added StrikeLabel user control Added header image

---
+++ VeryCustomUI/VeryCustomUI/Views/StrikeLabel.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace VeryCustomUI.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class StrikeLabel : ContentView
+	{
+        public static readonly BindableProperty TextProperty = BindableProperty.Create(nameof(Text), typeof(string), typeof(StrikeLabel));
+
+        public string Text
+        {
+            get
+            {
+                return (string)GetValue(TextProperty);
+            }
+            set
+            {
+                SetValue(TextProperty, value);
+            }
+        }
+
+        public StrikeLabel()
+		{
+			InitializeComponent();
+
+            //this.BindingContext = this;
+		}
+	}
+}
